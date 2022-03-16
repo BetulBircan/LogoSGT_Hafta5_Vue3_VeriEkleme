256f7b880cc0bd27abe7058e96ce6c3888a1cf9e
Betul Bircan <bircan.betul_1996@hotmail.com>
2022-03-16 17:09:50 +0300

    sbadmin sayfası componentlere ayrıldı

--- src/components/PageContent.vue
+++ src/components/PageContent.vue
@@ -1,6 +1,41 @@
 <template>
     <div  class="container-fluid">
-       <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
+       <div class="row">
+            <div class="col-md-6">
+              <div v-for="item in todoData" :key="item.id">
+                <div class="col-md-11" v-on:click="showAlert(item.id)">
+                  <div v-if="!item.completed">
+                    <div class="alert alert-success" role="alert">
+                      Adı: {{ item.name}}
+                      <br>
+                      E-mail: {{ item.email}}
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-1">
+                  <button class="btn btn-danger" v-on:click="removeTodo(item.id)">Sil</button>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div v-for="item in todoData2" :key="item.id">
+                <div class="col-md-11" v-on:click="showAlert2(item.id)">
+                  
+                  <div v-if="!item.completed">
+                    <div class="alert alert-danger" role="alert">
+                      Adı: {{ item.name}}
+                      <br>
+                      E-mail: {{ item.email}}
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-1">
+                  <button class="btn btn-danger" v-on:click="removeTodo2(item.id)">Sil</button>
+                </div>
+              </div>
+            </div>
+            <!-- /.container-fluid -->
+          </div>
     </div>
 </template>
 
